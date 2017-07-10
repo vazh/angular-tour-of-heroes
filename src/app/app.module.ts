ecdca68e94177cfcc1936b523207579cfac9cfc3
Muhammad Hariz Faza <m.hariz.faza@gmail.com>
2017-07-10 16:25:03 +0700

    feat(*): add package for in memory data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { HttpModule } from "@angular/http";
+
+import { AppRoutingModule } from "./app-routing.module";
+
+import { InMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from "./app-routing.module";
 import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -20,6 +24,8 @@ import { HeroService } from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
