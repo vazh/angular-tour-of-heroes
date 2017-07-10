9f11f436dea8cd63e65ef4281c1407ab88556561
Muhammad Hariz Faza <m.hariz.faza@gmail.com>
2017-07-10 10:00:27 +0700

    feat(*): separate dashboard template

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-dashboard',
-    template: '<h3>My Dashboard</h3>'
+    templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent { }
